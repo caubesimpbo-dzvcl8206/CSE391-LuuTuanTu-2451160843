921c52fef129f36791fe71fa258f42085b64231c
caubesimpbo-dzvcl8206 <tuluu6459@gmail.com>
2026-05-27 14:19:53 +0700

    PBT_07:Hoàn thành phần B và thêm screenshots phần B

---
+++ PBT_07/guess.js
@@ -0,0 +1,62 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let attempts = 0;
+
+let maxAttempts = 7;
+
+let guessedNumbers = [];
+
+while (attempts < maxAttempts) {
+
+    let input = prompt("Nhập số từ 1 đến 100:");
+
+    if (input === null) {
+        alert("Bạn đã thoát game!");
+        break;
+    }
+
+    let guess = Number(input);
+
+    if (
+        isNaN(guess) ||
+        guess < 1 ||
+        guess > 100
+    ) {
+        alert("Vui lòng nhập số từ 1 đến 100!");
+        continue;
+    }
+
+    if (guessedNumbers.includes(guess)) {
+        alert("Bạn đã đoán số này rồi!");
+        continue;
+    }
+
+    guessedNumbers.push(guess);
+
+    attempts++;
+
+    if (guess === randomNumber) {
+
+        alert(
+            `Đúng rồi! Bạn đoán đúng sau ${attempts} lần!`
+        );
+
+        break;
+    }
+    else if (guess < randomNumber) {
+
+        alert("Cao hơn!");
+
+    }
+    else {
+
+        alert("Thấp hơn!");
+    }
+
+    if (attempts === maxAttempts) {
+
+        alert(
+            `Bạn đã thua! Đáp án là ${randomNumber}`
+        );
+    }
+}
